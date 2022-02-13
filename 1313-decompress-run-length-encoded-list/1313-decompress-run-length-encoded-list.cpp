e305d7e48a82836241bb0f692e6bce28f77fd133
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-13 21:54:55 +0530

    Time: 7 ms (65.11%), Space: 10.2 MB (9.34%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -3,11 +3,22 @@ public:
     vector<int> decompressRLElist(vector<int>& nums) {
         vector<int> answer;
         for (int i = 0; i < nums.size(); i+=2) {
-            int freq = nums[i], val = nums[i+1];
-            while (freq--) {
-                answer.push_back(val);
-            }
+            answer.insert(answer.end(), nums[i], nums[i+1]);
         }
         return answer;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> decompressRLElist(vector<int>& nums) {
+//         vector<int> answer;
+//         for (int i = 0; i < nums.size(); i+=2) {
+//             int freq = nums[i], val = nums[i+1];
+//             while (freq--) {
+//                 answer.push_back(val);
+//             }
+//         }
+//         return answer;
+//     }
+// };
