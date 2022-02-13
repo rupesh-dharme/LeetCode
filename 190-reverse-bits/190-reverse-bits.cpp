0e95d40ec1ed0885b519e8be36da11e0af0f8d1c
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-13 12:14:20 +0530

    Time: 4 ms (47.04%), Space: 6 MB (10.59%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) { // 11111111111111111111111111111101
-        bitset<32> b(n); 
+    uint32_t reverseBits(uint32_t n) {
+        bitset <32> b(n);
         string s = b.to_string();
-        reverse(s.begin(), s.end()); // 10111111111111111111111111111111
-        return s[0]=='0' ? stoi(s, 0, 2) : (2147483648 + stoi(s.substr(1, s.size()-1), 0, 2)); // check because of unsigned integer.
+        reverse(s.begin(), s.end());
+        bitset <32> b2(s);
+        return b2.to_ulong();
     }
 };
+
+
+// class Solution {
+// public:
+//     uint32_t reverseBits(uint32_t n) { // 11111111111111111111111111111101
+//         bitset<32> b(n); 
+//         string s = b.to_string();
+//         reverse(s.begin(), s.end()); // 10111111111111111111111111111111
+//         return s[0]=='0' ? stoi(s, 0, 2) : (2147483648 + stoi(s.substr(1, s.size()-1), 0, 2)); // check because of unsigned integer.
+//     }
+// };
