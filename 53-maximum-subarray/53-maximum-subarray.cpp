8df55085dee205cc984246675c60c9669c4f9cc2
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-13 14:31:39 +0530

    Time: 153 ms (47.59%), Space: 67.8 MB (11.64%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int mx = INT_MIN;
+        int sum = 0;
+        for (auto num : nums) {
+            sum += num;
+            mx = max(sum, mx);
+            if (sum < 0) sum = 0;
+        }
+        return mx;
+    }
+};
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) { //       TLE
+//         int mx = INT_MIN, n = nums.size();
+//         for (int i = 0; i < n; i++) {
+//             int sum = 0;
+//             for (int j = i; j < n; j++) {
+//                 sum += nums[j];
+//                 mx = mx > sum ? mx : sum;
+//             }
+//         }
+//         return mx;
+//     }
+// };
