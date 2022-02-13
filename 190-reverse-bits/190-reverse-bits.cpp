3344aab4da029ce0b090c3d1f85a80bca039c9c4
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-13 12:46:10 +0530

    Time: 3 ms (61.81%), Space: 5.9 MB (81.74%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        bitset <32> b(n);
-        string s = b.to_string();
-        reverse(s.begin(), s.end());
-        bitset <32> b2(s);
-        return b2.to_ulong();
+        n = ((0xffff0000 & n) >> 16) | ((0x0000ffff & n) <<16);
+        n = ((0xff00ff00 & n) >> 8) | ((0x00ff00ff & n) << 8);
+        n = ((0xf0f0f0f0 & n) >> 4) | ((0x0f0f0f0f & n) << 4);
+        n = ((0xcccccccc & n) >> 2) | ((0x33333333 & n) << 2);
+        n = ((0xaaaaaaaa & n) >> 1) | ((0x55555555 & n) << 1);
+        return n;
     }
 };
+
+
+// class Solution {
+// public:
+//     uint32_t reverseBits(uint32_t n) {
+//         bitset <32> b(n);
+//         string s = b.to_string();
+//         reverse(s.begin(), s.end());
+//         bitset <32> b2(s);
+//         return b2.to_ulong();
+//     }
+// };
 
 
 // class Solution {
