6348365964a2ae40fe6089486099df69676b6f3b
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-15 11:02:54 +0530

    Time: 29 ms (47.53%), Space: 17 MB (50.83%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
-public: 
+public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for (auto num : nums) ans ^= num;
-        return ans;
+        int xr = 0;
+        for (auto num : nums) xr ^= num;
+        return xr;
     }
 };
