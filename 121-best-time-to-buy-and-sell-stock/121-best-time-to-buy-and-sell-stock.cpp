fa4ab2d3e935a142e8eb5d92e8acd662201da3db
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-15 11:27:43 +0530

    Time: 246 ms (17.81%), Space: 93.4 MB (53.86%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -13,7 +13,7 @@ public:
 // class Solution {
 // public:
 //     int maxProfit(vector<int>& prices) {
-//         int mx = INT_MIN;
+//         int mx = 0;
 //         for (int i = 0; i < prices.size()-1; i++) {
 //             for (int j = i+1; j < prices.size(); j++) {
 //                 mx = max(mx, prices[j] - prices[i]);
