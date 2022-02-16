4fae62d2f3859570f787ab5dcff429e7cb3abae3
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-16 12:33:08 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (72.29%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res = {{1}};
-        for (int i = 1; i < numRows; i++) {
-            vector temp1 = res.back();
-            temp1.push_back(0);
-            reverse(temp1.begin(), temp1.end());
-            vector temp2 = res.back();
-            temp2.push_back(0);
-            for (int j = 0; j < temp1.size(); j++) {
-                temp1[j] += temp2[j];
+        vector<vector<int>> res(numRows);
+        for (int i = 0; i < numRows; i++) {
+            res[i].resize(i+1);
+            res[i][0] = res[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                res[i][j] = res[i-1][j-1] + res[i-1][j];
             }
-            res.push_back(temp1);
         }
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows) {
+//         vector<vector<int>> res = {{1}};
+//         for (int i = 1; i < numRows; i++) {
+//             vector temp1 = res.back();
+//             temp1.push_back(0);
+//             reverse(temp1.begin(), temp1.end());
+//             vector temp2 = res.back();
+//             temp2.push_back(0);
+//             for (int j = 0; j < temp1.size(); j++) {
+//                 temp1[j] += temp2[j];
+//             }
+//             res.push_back(temp1);
+//         }
+//         return res;
+//     }
+// };
