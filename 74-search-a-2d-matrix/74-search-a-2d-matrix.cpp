3ec7e605d30185474df34ac7ac349f503c19dc52
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-17 14:36:01 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (78.65%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,11 +2,26 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
-        int i = 0;
-        for (; i < m; i++) {
-            if (matrix[i][0] > target) break;
+        int l = 0, r = m*n-1, mid;
+        while (l <= r) {
+            mid = l + (r-l)/2;
+            if (matrix[mid/n][mid%n] == target) return true;
+            else if (matrix[mid/n][mid%n] < target) l = mid+1;
+            else r = mid-1;
         }
-        if (i > 0) i -= 1;
-        return binary_search(matrix[i].begin(), matrix[i].end(), target);
+        return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m = matrix.size(), n = matrix[0].size();
+//         int i = 0;
+//         for (; i < m; i++) {
+//             if (matrix[i][0] > target) break;
+//         }
+//         if (i > 0) i -= 1;
+//         return binary_search(matrix[i].begin(), matrix[i].end(), target);
+//     }
+// };
