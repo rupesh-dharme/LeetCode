4b19adca841705f8a9fcded75bb9cb4dcbeb6b65
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-20 23:01:52 +0530

    Time: 110 ms (39.82%), Space: 30.9 MB (80.47%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -3,8 +3,8 @@ public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
         unordered_map<string, int> ump = {{"type", 0}, {"color", 1}, {"name", 2}};
         int cnt = 0;
-        for (auto item : items) {
-            if (ruleValue == item[ump[ruleKey]]) cnt++;
+        for (int i = 0; i < items.size(); i++) {
+            if (ruleValue == items[i][ump[ruleKey]]) cnt++;
         }
         return cnt;
     }
