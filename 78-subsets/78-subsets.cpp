e1e1d4594f1f2c2825492a469d6efef63c84b9b2
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-02-22 17:27:11 +0530

    Time: 3 ms (69.09%), Space: 10.8 MB (28.72%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,17 +1,65 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> res;
-        vector<int> arr;
-        recurse(nums, 0, arr, res, n);
-        return res;
+        vector<vector<int>> subsets;
+        vector<int> subset;
+        recurse(nums, 0, subset, subsets);
+        return subsets;
     }
-    void recurse(vector<int>& nums, int i, vector<int>& arr, vector<vector<int>>& res, int n) {
-        if (i >= n) {res.push_back(arr); return;}
-        recurse(nums, i+1, arr, res, n);
-        arr.push_back(nums[i]);
-        recurse(nums, i+1, arr, res, n);
-        arr.pop_back();
+    void recurse(vector<int>& nums, int i, vector<int>& subset, vector<vector<int>>& subsets) {
+        if (i == nums.size()) {subsets.push_back(subset); return;}
+        recurse(nums, i+1, subset, subsets);
+        subset.push_back(nums[i]);
+        recurse(nums, i+1, subset, subsets);
+        subset.pop_back();
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> res;
+//         vector<int> arr;
+//         recurse(nums, 0, arr, res, n);
+//         return res;
+//     }
+//     void recurse(vector<int>& nums, int i, vector<int>& arr, vector<vector<int>>& res, int n) {
+//         if (i >= n) {res.push_back(arr); return;}
+//         recurse(nums, i+1, arr, res, n);
+//         arr.push_back(nums[i]);
+//         recurse(nums, i+1, arr, res, n);
+//         arr.pop_back();
+//     }
+// };
