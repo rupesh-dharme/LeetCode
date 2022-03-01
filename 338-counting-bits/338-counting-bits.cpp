a73392723575704eac103157ddb7490dd9c26e4b
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-01 10:19:46 +0530

    Time: 3 ms (95.58%), Space: 7.7 MB (98.10%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> answer(n+1, 0);
+        vector<int> result(n+1, 0);
         for (int i = 1; i <= n; i++) {
-            answer[i] = answer[i&(i-1)] + 1;
+            result[i] = result[i&(i-1)]+1;
         }
-        return answer;
+        return result;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         vector<int> answer(n+1, 0);
+//         for (int i = 1; i <= n; i++) {
+//             answer[i] = answer[i&(i-1)] + 1;
+//         }
+//         return answer;
+//     }
+// };
 
 // class Solution {
 // public:
