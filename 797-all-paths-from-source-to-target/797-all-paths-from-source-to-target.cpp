442000c51f5085c8b8d4ebf492c8ff025b3d7c3a
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-10 12:51:13 +0530

    Time: 12 ms (90.01%), Space: 10.7 MB (71.63%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -4,17 +4,17 @@ public:
         vector<vector<int>> res;
         vector<int> path;
         path.push_back(0);
-        allPaths(graph, 0, res, path);
+        recurse(0, graph.size()-1, graph, path, res);
         return res;
     }
-    void allPaths(vector<vector<int>>& graph, int node, vector<vector<int>>& res, vector<int>& path) {
-        if (node == graph.size()-1) {
+    void recurse(int i, int d, vector<vector<int>>& graph, vector<int>& path, vector<vector<int>> & res) {
+        if (i == d) {
             res.push_back(path);
             return;
         }
-        for (auto idx : graph[node]) {
-            path.push_back(idx);
-            allPaths(graph, idx, res, path);
+        for (auto node : graph[i]) {
+            path.push_back(node);
+            recurse(node, d, graph, path, res);
             path.pop_back();
         }
     }
