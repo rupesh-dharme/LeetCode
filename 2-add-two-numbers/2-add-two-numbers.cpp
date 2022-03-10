89539d7eb3f6374b8358386de8feecd49a671324
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-10 17:17:34 +0530

    Time: 54 ms (47.04%), Space: 71.4 MB (50.05%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,40 +11,24 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* res = new ListNode();
-        ListNode* curr = new ListNode();
-        curr->next = res;
-        int c = 0;
-        while(l1 && l2) {
-            int val = l1->val + l2->val + c;
-            curr = curr->next;
-            c = val/10;
-            val %= 10;
-            curr->val = val;
-            curr->next = new ListNode();
-            l1 = l1->next;
-            l2 = l2->next;
+        ListNode* dummy=new ListNode(0);
+        ListNode* head=dummy;
+        int sum=0;
+        while(l1 || l2 || sum){
+            if(l1){
+               sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2){
+sum+=l2->val;
+                l2=l2->next;
+            }
+            
+            ListNode* temp=new ListNode(sum%10);
+            head->next=temp;
+            head=temp;
+            sum/=10;
         }
-        while (l1) {
-            int val = l1->val + c;
-            c = val/10;
-            curr = curr->next;
-            val %= 10;
-            curr->val = val;
-            curr->next = new ListNode();
-            l1 = l1->next;
-        }
-        while (l2) {
-            int val = l2->val + c;
-            c = val/10;
-            val %= 10;
-            curr = curr->next;
-            curr->val = val;
-            curr->next = new ListNode();
-            l2 = l2->next;
-        }
-        if (c) curr->next = new ListNode(c);
-        else curr->next = NULL;
-        return res;
+        return dummy->next;
     }
 };
