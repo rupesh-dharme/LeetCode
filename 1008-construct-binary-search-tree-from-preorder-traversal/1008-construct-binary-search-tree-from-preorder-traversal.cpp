d0dd1868884624310d2929e159f5f44624044c83
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-12 14:35:57 +0530

    Time: 4 ms (82.33%), Space: 13.8 MB (47.81%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -12,21 +12,36 @@
 class Solution {
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return create(preorder, 0, preorder.size()-1);
+        int i = 0;
+        return create(preorder, i, INT_MAX);
     }
-    TreeNode* create(vector<int>& pre, int s, int e) {
-        if (s > e) return NULL;
-        if (s == e) {
-            TreeNode* root = new TreeNode(pre[s]);
-            return root;
-        }
-        int mid = s+1;
-        for (; mid <= e; mid++) {
-            if (pre[mid] > pre[s]) break;
-        }
-        TreeNode* root = new TreeNode(pre[s]);
-        root->left = create(pre, s+1, mid-1);
-        root->right = create(pre, mid, e);
+    TreeNode* create(vector<int>& pre, int& i, int upper) {
+        if (i >= pre.size() || pre[i] > upper) return NULL;
+        TreeNode* root = new TreeNode(pre[i++]);
+        root->left = create(pre, i, root->val);
+        root->right = create(pre, i, upper);
         return root;
     }
 };
+
+// class Solution {
+// public:
+//     TreeNode* bstFromPreorder(vector<int>& preorder) {
+//         return create(preorder, 0, preorder.size()-1);
+//     }
+//     TreeNode* create(vector<int>& pre, int s, int e) {
+//         if (s > e) return NULL;
+//         if (s == e) {
+//             TreeNode* root = new TreeNode(pre[s]);
+//             return root;
+//         }
+//         int mid = s+1;
+//         for (; mid <= e; mid++) {
+//             if (pre[mid] > pre[s]) break;
+//         }
+//         TreeNode* root = new TreeNode(pre[s]);
+//         root->left = create(pre, s+1, mid-1);
+//         root->right = create(pre, mid, e);
+//         return root;
+//     }
+// };
