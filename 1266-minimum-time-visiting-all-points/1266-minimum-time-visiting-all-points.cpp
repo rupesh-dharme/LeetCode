a391bc46d9eeee82c4dee10160d24cb52f9b1b47
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-13 16:52:32 +0530

    Time: 23 ms (12.67%), Space: 10.2 MB (65.73%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -4,10 +4,7 @@ public:
         int n = points.size(), time = 0;
         if (n == 1) return time;
         for (int i = 0; i < n-1; i++) {
-            int cx = points[i][0];
-            int cy = points[i][1];
-            int nx = points[i+1][0];
-            int ny = points[i+1][1];
+            int cx = points[i][0], cy = points[i][1], nx = points[i+1][0], ny = points[i+1][1];
             int m = min(abs(cx-nx), abs(cy-ny));
             time += m;
             time += abs(cx-nx) + abs(cy-ny) - 2*m;
