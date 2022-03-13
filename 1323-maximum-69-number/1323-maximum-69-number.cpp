42fbd974c34af2f8cfff90242ac09edd188daf4e
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-13 18:23:05 +0530

    Time: 3 ms (28.99%), Space: 5.9 MB (82.66%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+        for(auto& ch : s) if (ch == '6') {ch+=3; break;}
+        return stoi(s);
+    }
+};
