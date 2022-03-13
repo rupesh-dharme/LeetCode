1da50874defbd2a1607eac386f44c2c755b78d4f
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-13 15:39:44 +0530

    Time: 17 ms (17.97%), Space: 8.8 MB (68.47%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
-        unordered_map<string, int> um;
-        for (int i = 0; i < word.size(); i++) {
-            string temp;
-            for (int j = i; j < word.size(); j++) {
-                temp += word[j];
-                um[temp] = 1;
-            }
-        }
         int count = 0;
-        for (auto& pattern : patterns) if (um.find(pattern) != um.end()) count++;
+        for (auto& pattern : patterns) if (word.find(pattern) != -1) count++;
         return count;
     }
 };
+
+// class Solution {
+// public:
+//     int numOfStrings(vector<string>& patterns, string word) {
+//         unordered_map<string, int> um;
+//         for (int i = 0; i < word.size(); i++) {
+//             string temp;
+//             for (int j = i; j < word.size(); j++) {
+//                 temp += word[j];
+//                 um[temp] = 1;
+//             }
+//         }
+//         int count = 0;
+//         for (auto& pattern : patterns) if (um.find(pattern) != um.end()) count++;
+//         return count;
+//     }
+// };
