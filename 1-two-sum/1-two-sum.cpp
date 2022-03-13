503e2d77354b2f0a87d63996e5787470cab582f8
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-13 12:15:40 +0530

    Time: 8 ms (95.92%), Space: 10.8 MB (50.97%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hash;
-        for (int i = 0; i < nums.size(); i++) {
-            if (hash.find(target - nums[i]) != hash.end()) {
-                return {hash[target - nums[i]], i};
-            }
-            else hash[nums[i]] = i;
+        unordered_map<int, int> um;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (um.find(target-nums[i]) != um.end()) return {um[target - nums[i]], i};
+            else um[nums[i]] = i;
         }
-        return {0, 0};
+        return {-1, -1};
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> hash;
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (hash.find(target - nums[i]) != hash.end()) {
+//                 return {hash[target - nums[i]], i};
+//             }
+//             else hash[nums[i]] = i;
+//         }
+//         return {0, 0};
+//     }
+// };
