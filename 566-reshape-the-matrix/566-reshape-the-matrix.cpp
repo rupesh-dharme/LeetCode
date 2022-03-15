142814b047cea77fd1b0cbd1e73440bb2c5ebc1f
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-15 16:00:04 +0530

    Time: 14 ms (53.17%), Space: 10.6 MB (94.78%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        int m = mat.size(), n = mat[0].size(), p, ri, ci;
-        if (m*n != r*c) return mat;
-        vector<vector<int>> answer(r, vector<int> (c, 0));
+        int m = mat.size(), n = mat[0].size();
+        if (m * n != r * c) return mat;
+        vector<vector<int>> answer(r, vector<int>(c, 0));
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                p = (i*c) + j;
-                ri = p/n;
-                ci = p%n;
-                answer[i][j] = mat[ri][ci];
+                int idx = i * c + j;
+                answer[i][j] = mat[idx/n][idx%n];
             }
         }
         return answer;
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+//         int m = mat.size(), n = mat[0].size(), p, ri, ci;
+//         if (m*n != r*c) return mat;
+//         vector<vector<int>> answer(r, vector<int> (c, 0));
+//         for (int i = 0; i < r; i++) {
+//             for (int j = 0; j < c; j++) {
+//                 p = (i*c) + j;
+//                 ri = p/n;
+//                 ci = p%n;
+//                 answer[i][j] = mat[ri][ci];
+//             }
+//         }
+//         return answer;
+//     }
+// };
