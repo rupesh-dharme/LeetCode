bd7218c29f58b01779113ef3f2e0de1d09f24e77
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-03-27 11:36:59 +0530

    Time: 12 ms (83.53%), Space: 10.6 MB (19.79%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -4,7 +4,7 @@ public:
         int n = mat.size();
         set<pair<int, int>> v;
         for (int i = 0; i < n; i++) {
-            v.insert({accumulate(mat[i].begin(), mat[i].end(), 0), i});
+            v.insert({count(mat[i].begin(), mat[i].end(), 1), i});
         }
         vector<int> result;
         for (auto it = v.begin(); k > 0 && it != v.end(); k--, it++) {
