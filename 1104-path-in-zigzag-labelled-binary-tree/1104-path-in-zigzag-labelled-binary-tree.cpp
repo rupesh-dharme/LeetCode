496ce6d754f6459c63e572f9720aadb0658032ee
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-04-18 17:29:39 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (16.51%) - LeetHub

---
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> pathInZigZagTree(int label) {
+        vector<int> result;
+        result.push_back(label);
+        int height=log2(label);
+        while(height!=0){
+            int left=pow(2,height-1);
+            int right=pow(2,height)-1;
+            label=left+(right-label/2);
+            result.push_back(label);
+            height--;
+        }
+        
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
