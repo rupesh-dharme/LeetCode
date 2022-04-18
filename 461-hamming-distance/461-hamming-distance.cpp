3a74b976a9c6b58c271c9b378ba9f98353556794
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-04-18 12:19:02 +0530

    Time: 0 ms (100.00%), Space: 6 MB (27.39%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int distance = 0;
-        // while (x && y) {
-        //     if ((x & 1) ^ (y & 1)) distance++;
-        //     x >> 1;
-        //     y >> 1;
-        // }
         int num = x ^ y;
         return __builtin_popcount(num);
-        // return distance;
     }
 };
+
+// class Solution {
+// public:
+//     int hammingDistance(int x, int y) {
+//         int distance = 0;
+//         // while (x && y) {
+//         //     if ((x & 1) ^ (y & 1)) distance++;
+//         //     x >> 1;
+//         //     y >> 1;
+//         // }
+//         int num = x ^ y;
+//         return __builtin_popcount(num);
+//         // return distance;
+//     }
+// };
