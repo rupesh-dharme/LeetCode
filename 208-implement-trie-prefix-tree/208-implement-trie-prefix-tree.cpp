43dede8cc567467141f1e0a1d1941e83392651a3
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-05 11:15:22 +0530

    Time: 69 ms (74.59%), Space: 45 MB (66.71%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,131 @@
+class Node {
+private:
+    Node* links[26];
+    bool flag;
+public:
+    bool contains(char ch) {
+        return links[ch - 'a'];
+    }
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+    void setEnd() {
+        flag = true;
+    }
+    bool isSet() {
+        return flag;
+    }
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!node->contains(word[i])) {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!node->contains(word[i])) return false;
+            node = node->get(word[i]);
+        }
+        return node->isSet();
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for (int i = 0; i < prefix.size(); i++) {
+            if (!node->contains(prefix[i])) return false;
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
+
+// class Node {
+// private:
+//     Node* links[26];
+//     bool flag;
+// public:
+//     bool contains(char ch) {
+//         return (links[ch - 'a'] != NULL);
+//     }
+//     void put(char ch, Node* node) {
+//         links[ch - 'a'] = node;
+//     }
+//     Node* get(char ch) {
+//         return links[ch - 'a'];
+//     }
+//     void setEnd() {
+//         flag = true;
+//     }
+//     bool isSet() {
+//         return flag;
+//     }
+// };
+
+// class Trie {
+// private:
+//     Node* root;
+// public:
+//     Trie() {
+//         root = new Node();
+//     }
+    
+//     void insert(string word) {
+//         Node* node = root;
+//         for (int i = 0; i < word.size(); i++) {
+//             if (!node->contains(word[i])) {
+//                 node->put(word[i], new Node());
+//             }
+//             node = node->get(word[i]);
+//         }
+//         node->setEnd();
+//     }
+    
+//     bool search(string word) {
+//         Node* node = root;
+//         for (int i = 0; i < word.size(); i++) {
+//             if (!node->contains(word[i])) {
+//                 return false;
+//             }
+//             node = node->get(word[i]);
+//         }
+//         return node->isSet();
+//     }
+    
+//     bool startsWith(string prefix) {
+//         Node* node = root;
+//         for (int i = 0; i < prefix.size(); i++) {
+//             if (!node->contains(prefix[i])) {
+//                 return false;
+//             }
+//             node = node->get(prefix[i]);
+//         }
+//         return true;
+//     }
+// };
