bcec5397304f3570470b06ebbd1f1df0405cd35c
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-11 08:59:07 +0530

    Time: 175 ms (7.03%), Space: 27.7 MB (9.63%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (auto &str : strs) {
+            vector<int> freq(26, 0);
+            for (auto ch : str) freq[ch - 'a']++;
+            string key;
+            for (int i = 0; i < 26; i++) {
+                key += '_';
+                key += to_string(freq[i]);
+            }
+            if (mp.count(key)) mp[key].push_back(str);
+            else mp[key] = {str};
+        }
+        vector<vector<string>> result;
+        for (auto &[key, v] : mp) {
+            result.push_back(v);
+        }
+        return result;
+    }
+};
