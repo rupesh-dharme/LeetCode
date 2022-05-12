20f6d501657d61c698342b878d2b2a2f4a6f67a6
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-12 14:20:45 +0530

    Time: 10 ms (64.25%), Space: 11.5 MB (20.10%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,23 +1,47 @@
 class Solution {
+private:
+    vector<vector<int>> result;
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> result;
-        permute(nums, nums.size(), 0, result);
+        recurse(nums, 0, nums.size());
         return result;
     }
-    void permute(vector<int>& nums, int n, int i, vector<vector<int>>& result) {
+    void recurse(vector<int> &nums, int i, int n) {
         if (i == n) {
             result.push_back(nums);
             return;
         }
-        unordered_map<int, int> count;
+        unordered_set<int> st;
         for (int j = i; j < n; j++) {
-            if (count[nums[j]] == 1) continue;
-            count[nums[j]] = 1;
-            swap(nums[j], nums[i]);
-            permute(nums, n, i+1, result);
-            swap(nums[j], nums[i]);
+            if (st.count(nums[j])) continue;
+            st.insert(nums[j]);
+            swap(nums[i], nums[j]);
+            recurse(nums, i+1, n);
+            swap(nums[i], nums[j]);
         }
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         vector<vector<int>> result;
+//         permute(nums, nums.size(), 0, result);
+//         return result;
+//     }
+//     void permute(vector<int>& nums, int n, int i, vector<vector<int>>& result) {
+//         if (i == n) {
+//             result.push_back(nums);
+//             return;
+//         }
+//         unordered_map<int, int> count;
+//         for (int j = i; j < n; j++) {
+//             if (count[nums[j]] == 1) continue;
+//             count[nums[j]] = 1;
+//             swap(nums[j], nums[i]);
+//             permute(nums, n, i+1, result);
+//             swap(nums[j], nums[i]);
+//         }
+//     }
+// };
