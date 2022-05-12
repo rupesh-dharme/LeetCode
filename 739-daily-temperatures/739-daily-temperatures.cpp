713475356792bc5d7cf3a20dcae5fb868132827d
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-12 10:52:08 +0530

    Time: 184 ms (78.47%), Space: 84.9 MB (99.71%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
-        stack<pair<int, int>> stk;
         int n = temp.size();
         vector<int> result(n, 0);
-        stk.push({temp[0], 0});
-        for (int i = 1; i < n; i++) {
-            while (stk.size() && stk.top().first < temp[i]) {
-                int ind = stk.top().second;
-                result[ind] = i - ind;
-                stk.pop();
+        for (int i = n-1; i >= 0; --i) {
+            int j = i+1;
+            while (j < n && temp[i] >= temp[j]) {
+                if (result[j] > 0) j += result[j];
+                else j = n;
             }
-            stk.push({temp[i], i});
+            if (j < n) result[i] = j - i;
         }
         return result;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temp) {
+//         stack<pair<int, int>> stk;
+//         int n = temp.size();
+//         vector<int> result(n, 0);
+//         stk.push({temp[0], 0});
+//         for (int i = 1; i < n; i++) {
+//             while (stk.size() && stk.top().first < temp[i]) {
+//                 int ind = stk.top().second;
+//                 result[ind] = i - ind;
+//                 stk.pop();
+//             }
+//             stk.push({temp[i], i});
+//         }
+//         return result;
+//     }
+// };
