54d1b6bcaf7cf8b542835c22639926b4e9c7b41a
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-14 09:48:16 +0530

    Time: 50 ms (44.33%), Space: 24 MB (61.06%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n = nums.size(), i;
-        stack<pair<int, int>> stk;
-        unordered_set<int> done, ind;
+        int n = nums.size();
         vector<int> res(n, -1);
-        for (int j = 0; j < 2 * n; j++) {
-            i = j % n;
-            while (stk.size() && stk.top().first < nums[i]) {
-                done.insert(i);
-                res[stk.top().second] = nums[i];
-                stk.pop();
+        stack<int> stack;
+        for (int i = 0; i < 2*n; i++) {
+            while (stack.size() && nums[stack.top()] < nums[i%n]) {
+                res[stack.top()] = nums[i%n];
+                stack.pop();
             }
-            if (ind.count(i) == 0) {
-                ind.insert(i);
-                stk.push({nums[i], i});
-            }
+            stack.push(i%n);
         }
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         int n = nums.size(), i;
+//         stack<pair<int, int>> stk;
+//         unordered_set<int> done, ind;
+//         vector<int> res(n, -1);
+//         for (int j = 0; j < 2 * n; j++) {
+//             i = j % n;
+//             while (stk.size() && stk.top().first < nums[i]) {
+//                 done.insert(i);
+//                 res[stk.top().second] = nums[i];
+//                 stk.pop();
+//             }
+//             if (ind.count(i) == 0) {
+//                 ind.insert(i);
+//                 stk.push({nums[i], i});
+//             }
+//         }
+//         return res;
+//     }
+// };
