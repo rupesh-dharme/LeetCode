2de4a8306b9db0030b835d02ff1dd34e89b25db1
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-24 11:06:19 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.81%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stk;
-        for (auto& ch : s) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stk.push(ch);
-            }
+        stack<char> stack;
+        unordered_map<char, char> mp = {{']', '['}, {')', '('}, {'}', '{'}};
+        for (auto ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') stack.push(ch);
             else {
-                if (stk.empty()) return false;
-                if (ch == stk.top() + 1 || ch == stk.top() + 2) {
-                    stk.pop();
-                } else return false;
+                if (stack.size() && stack.top() == mp[ch]) stack.pop();
+                else return false;
             }
         }
-        return stk.empty();
+        return stack.empty();
     }
 };
+
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> stk;
+//         for (auto& ch : s) {
+//             if (ch == '(' || ch == '{' || ch == '[') {
+//                 stk.push(ch);
+//             }
+//             else {
+//                 if (stk.empty()) return false;
+//                 if (ch == stk.top() + 1 || ch == stk.top() + 2) {
+//                     stk.pop();
+//                 } else return false;
+//             }
+//         }
+//         return stk.empty();
+//     }
+// };
