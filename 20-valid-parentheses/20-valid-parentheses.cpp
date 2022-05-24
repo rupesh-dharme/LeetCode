abe8bcda49178111ef143dad51cae780fcadfdf9
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-24 12:51:12 +0530

    Time: 2 ms (46.99%), Space: 6.5 MB (10.81%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,3 +1,37 @@
+// class Solution {
+// private:
+//     unordered_map<char, char> mp = {{']', '['}, {')', '('}, {'}', '{'}};
+// public:
+//     bool isValid(string s) {
+//         // stack<char> stack;
+//         int top = -1;
+        
+//         for (int i = 0; i < s.size(); i++) {
+//             char ch = s[i];
+//             if (ch == '(' || ch == '[' || ch == '{') top = i;
+//             else {
+//                 if (top == -1 || s[top] != mp[ch]) return false;
+//                 else {
+//                     top = getTop(i-1, s);
+//                 }
+//             }
+//         }
+//         return top == -1;
+//     }
+//     int getTop(int i, string &s) {
+//         int right = 0;
+//         while (i >= 0) {
+//             if (mp.find(s[i]) != mp.end()) right++;
+//             else right--;
+            
+//             if (right < 0) return i;
+            
+//             i--;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     bool isValid(string s) {
