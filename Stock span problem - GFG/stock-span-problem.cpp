dd3c9d041280d536bb91cb411a71f2229a05b387
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-26 14:12:08 +0530

    Added solution - LeetHub

--- Stock span problem - GFG/stock-span-problem.cpp
+++ Stock span problem - GFG/stock-span-problem.cpp
@@ -10,25 +10,73 @@ class Solution
 {
     public:
     //Function to calculate the span of stockâ€™s price for all n days.
-    vector <int> calculateSpan(int price[], int n)
+    vector<int> calculateSpan(int price[], int n)
     {
-       stack<pair<int, int>> stack;
-       vector<int> result(n);
-       for (int i = 0; i < n; i++) {
-           if (!stack.size()) result[i] = -1;
-           else if (stack.top().first > price[i]) result[i] = stack.top().second;
-           else if (stack.top().first <= price[i]) {
-               while (stack.size() && stack.top().first <= price[i]) stack.pop();
-               if (stack.size()) result[i] = stack.top().second;
-               else result[i] = -1;
-           }
-           stack.push({price[i], i});
-       }
-       for (int i = 0; i < n; i++) {
-           result[i] = i - result[i];
-       }
-       return result;
+        stack<pair<int, int>> stack;
+        vector<int> res(n, -1);
+        for (int i = 0; i < n; i++) {
+            if (stack.empty()) {}
+            else if (stack.top().first > price[i]) res[i] = stack.top().second;
+            else if (stack.top().first <= price[i]) {
+                while (stack.size() && stack.top().first <= price[i]) stack.pop();
+                if (stack.size()) res[i] = stack.top().second;
+                else {}
+            }
+            stack.push({price[i], i});
+        }
+        for (int i = 0; i < n; i++) {
+            res[i] = i - res[i];
+        }
+        return res;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // vector <int> calculateSpan(int price[], int n)
+    // {
+    //   stack<pair<int, int>> stack;
+    //   vector<int> result(n);
+    //   for (int i = 0; i < n; i++) {
+    //       if (!stack.size()) result[i] = -1;
+    //       else if (stack.top().first > price[i]) result[i] = stack.top().second;
+    //       else if (stack.top().first <= price[i]) {
+    //           while (stack.size() && stack.top().first <= price[i]) stack.pop();
+    //           if (stack.size()) result[i] = stack.top().second;
+    //           else result[i] = -1;
+    //       }
+    //       stack.push({price[i], i});
+    //   }
+    //   for (int i = 0; i < n; i++) {
+    //       result[i] = i - result[i];
+    //   }
+    //   return result;
+    // }
 };
 
 
