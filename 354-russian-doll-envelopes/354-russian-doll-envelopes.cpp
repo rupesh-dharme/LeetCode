b8cf7c27bc15bb05fb814542549680da1acd39ff
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-26 15:17:27 +0530

    Time: 432 ms (68.50%), Space: 77.5 MB (70.81%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,26 +1,48 @@
 class Solution {
-    static bool cmp(vector<int>& a, vector<int>& b){
-        if(a[0]==b[0]) return a[1] > b[1];
-        return a[0] < b[0];
-    }
 public:
     int maxEnvelopes(vector<vector<int>>& env) {
-        int n = env.size();
+        sort(env.begin(), env.end(), [](const vector<int> &a, const vector<int> &b) {
+            if (a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
+        vector<int> temp;
+        temp.push_back(env[0][1]);
         
-        sort(env.begin(), env.end(), cmp);
-        
-      
-        vector<int> lis;
-        
-        for(int i = 0;i<env.size();i++){
-            int ele = env[i][1];
-            
-            int idx = lower_bound(lis.begin(), lis.end(), ele) - lis.begin();
-            
-            if(idx >= lis.size()) lis.push_back(ele);
-            else lis[idx] = ele;
+        for (int i = 1; i < env.size(); i++) {
+            if (env[i][1] > temp.back()) temp.push_back(env[i][1]);
+            else {
+                int ind = lower_bound(temp.begin(), temp.end(), env[i][1]) - temp.begin();
+                temp[ind] = env[i][1];
+            }
         }
         
-        return lis.size();
+        return temp.size();
     }
 };
+
+// class Solution {
+//     static bool cmp(vector<int>& a, vector<int>& b){
+//         if(a[0]==b[0]) return a[1] > b[1];
+//         return a[0] < b[0];
+//     }
+// public:
+//     int maxEnvelopes(vector<vector<int>>& env) {
+//         int n = env.size();
+        
+//         sort(env.begin(), env.end(), cmp);
+        
+      
+//         vector<int> lis;
+        
+//         for(int i = 0;i<env.size();i++){
+//             int ele = env[i][1];
+            
+//             int idx = lower_bound(lis.begin(), lis.end(), ele) - lis.begin();
+            
+//             if(idx >= lis.size()) lis.push_back(ele);
+//             else lis[idx] = ele;
+//         }
+        
+//         return lis.size();
+//     }
+// };
