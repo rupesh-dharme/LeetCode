9ea9b1950859ba7ef53da870d74dd8da162f267c
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-26 15:04:40 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -9,21 +9,36 @@ class Solution
 {
     public:
     //Function to find length of longest increasing subsequence.
-    int longestSubsequence(int n, int a[])
+    int longestSubsequence(int n, int a[]) // N logN Solution
     {
        // your code here
-       vector<int> lis(n, 1);
-       int ans = 1;
-       for (int i = 1; i < n; i++) {
-           for (int j = 0; j < i; j++) {
-               if (a[i] > a[j] && lis[i] <= lis[j]) {
-                   lis[i] = lis[j] + 1;
-                   ans = max(ans, lis[i]);
-               }
+       vector<int> temp;
+       temp.push_back(a[0]);
+       for (int i = 1; i < n; ++i) {
+           if (temp.back() < a[i]) temp.push_back(a[i]);
+           else {
+               int ind = lower_bound(temp.begin(), temp.end(), a[i]) - temp.begin();
+               temp[ind] = a[i];
            }
        }
-       return ans;
+       return temp.size();
     }
+    
+    // int longestSubsequence(int n, int a[])
+    // {
+    //   // your code here
+    //   vector<int> lis(n, 1);
+    //   int ans = 1;
+    //   for (int i = 1; i < n; i++) {
+    //       for (int j = 0; j < i; j++) {
+    //           if (a[i] > a[j] && lis[i] <= lis[j]) {
+    //               lis[i] = lis[j] + 1;
+    //               ans = max(ans, lis[i]);
+    //           }
+    //       }
+    //   }
+    //   return ans;
+    // }
 };
 
 // { Driver Code Starts.
