7637bfe1db19104237f311b591497549043dac70
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-30 20:52:13 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -11,13 +11,16 @@ using namespace std;
 class Solution{
   public:
     int cutRod(int price[], int n) {
-        vector<int> length(n);
-        iota(length.begin(), length.end(), 1);
-        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        int dp[n+1][n+1];
+        vector<int> arr(n);
+        iota(arr.begin(), arr.end(), 1);
+        
+        memset(dp, 0, sizeof dp);
+        
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (length[i-1] <= j) {
-                    dp[i][j] = max(dp[i-1][j], price[i-1] + dp[i][j - length[i-1]]);
+                if (arr[i-1] <= j) {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-arr[i-1]] + price[i-1]);
                 }
                 else {
                     dp[i][j] = dp[i-1][j];
@@ -27,6 +30,47 @@ class Solution{
         return dp[n][n];
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution{
+//   public:
+//     int cutRod(int price[], int n) {
+//         vector<int> length(n);
+//         iota(length.begin(), length.end(), 1);
+//         vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 if (length[i-1] <= j) {
+//                     dp[i][j] = max(dp[i-1][j], price[i-1] + dp[i][j - length[i-1]]);
+//                 }
+//                 else {
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+//         return dp[n][n];
+//     }
+// };
 
 // { Driver Code Starts.
 
