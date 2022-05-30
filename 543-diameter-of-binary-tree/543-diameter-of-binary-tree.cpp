ebd53d9bfecf68080f5ec5fb0bcca3b0461ab8e7
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-30 12:57:30 +0530

    Time: 21 ms (26.73%), Space: 20.3 MB (26.03%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -12,19 +12,36 @@
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0;
-        solve(root, diameter);
-        return diameter;
+        int mx = 0;
+        recurse(root, mx);
+        return mx;
     }
     
-    int solve(TreeNode* root, int &diameter) {
+    int recurse(TreeNode *root, int &mx) {
         if (!root) return 0;
-        
-        int l = solve(root->left, diameter);
-        int r = solve(root->right, diameter);
-        
-        int temp = max(l, r) + 1;
-        diameter = max(diameter, l + r);
-        return temp;
+        int l = recurse(root->left, mx);
+        int r = recurse(root->right, mx);
+        mx = max(mx, l + r);
+        return 1 + max(l, r);
     }
 };
+
+// class Solution {
+// public:
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         int diameter = 0;
+//         solve(root, diameter);
+//         return diameter;
+//     }
+    
+//     int solve(TreeNode* root, int &diameter) {
+//         if (!root) return 0;
+        
+//         int l = solve(root->left, diameter);
+//         int r = solve(root->right, diameter);
+        
+//         int temp = max(l, r) + 1;
+//         diameter = max(diameter, l + r);
+//         return temp;
+//     }
+// };
