e7a1469ad781500ac8aa7b2f0fae2cfa96ed0a80
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-30 18:32:41 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -12,21 +12,43 @@ public:
     bool isSubsetSum(vector<int>arr, int sum){
         int n = arr.size();
         bool dp[n+1][sum+1];
-        for (int i = 1; i <= sum; i++) dp[0][i] = false;
+        memset(dp, false, sizeof dp);
         for (int i = 0; i <= n; i++) dp[i][0] = true;
+        
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= sum; j++) {
                 if (arr[i-1] <= j) {
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j - arr[i-1]];
-                }
-                else {
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                } else {
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
+        
         return dp[n][sum];
     }
 };
+
+// class Solution{   
+// public:
+//     bool isSubsetSum(vector<int>arr, int sum){
+//         int n = arr.size();
+//         bool dp[n+1][sum+1];
+//         for (int i = 1; i <= sum; i++) dp[0][i] = false;
+//         for (int i = 0; i <= n; i++) dp[i][0] = true;
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= sum; j++) {
+//                 if (arr[i-1] <= j) {
+//                     dp[i][j] = dp[i-1][j] || dp[i-1][j - arr[i-1]];
+//                 }
+//                 else {
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+//         return dp[n][sum];
+//     }
+// };
 
 // class Solution{   
 // public:
