c61d09a9f841d8778e8a255f163699d4e5f6b68c
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-05-31 13:54:15 +0530

    Time: 9 ms (37.33%), Space: 16.4 MB (78.26%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -12,14 +12,27 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if (not root) return true;
         return isMirrored(root->left, root->right);
     }
-    bool isMirrored(TreeNode* left, TreeNode* right) {
-        if (!right && !left) return true;
-        if (right && left) {
-            return right->val == left->val && isMirrored(right->left, left->right) && isMirrored(right->right, left->left);
-        }
-        return false;
+    
+    bool isMirrored(TreeNode *left, TreeNode *right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        return left->val == right->val && isMirrored(left->left, right->right) && isMirrored(left->right, right->left);
     }
 };
+
+// class Solution {
+// public:
+//     bool isSymmetric(TreeNode* root) {
+//         if (not root) return true;
+//         return isMirrored(root->left, root->right);
+//     }
+//     bool isMirrored(TreeNode* left, TreeNode* right) {
+//         if (!right && !left) return true;
+//         if (right && left) {
+//             return right->val == left->val && isMirrored(right->left, left->right) && isMirrored(right->right, left->left);
+//         }
+//         return false;
+//     }
+// };
