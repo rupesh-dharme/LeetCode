4f28beebcdb6a0b38a277be1c86f4018c244b6f1
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-01 14:49:54 +0530

    Time: 17 ms (73.04%), Space: 13.7 MB (72.52%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,22 +1,42 @@
-
+class Compare {
+public:
+    bool operator() (const pair<int, int> &lhs, const pair<int, int> &rhs) {
+        return lhs.second > rhs.second;
+    }
+};
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> map;
-        for(int num : nums){
-            map[num]++;
+        unordered_map<int, int> um;
+        for (auto num : nums) um[num]++;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
+        for (auto &p : um) {
+            pq.push(p);
+            if (pq.size() > k) pq.pop();
         }
-        
         vector<int> res;
-        priority_queue<pair<int,int>> pq; 
-        for(auto it = map.begin(); it != map.end(); it++){
-            pq.push(make_pair(it->second, it->first));
-            if(pq.size() > (int)map.size() - k){
-                res.push_back(pq.top().second);
-                pq.pop();
-            }
+        while (pq.size()) {
+            res.push_back(pq.top().first);
+            pq.pop();
         }
         return res;
     }
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int,int> map;
+//         for(int num : nums){
+//             map[num]++;
+//         }
+        
+//         vector<int> res;
+//         priority_queue<pair<int,int>> pq; 
+//         for(auto it = map.begin(); it != map.end(); it++){
+//             pq.push(make_pair(it->second, it->first));
+//             if(pq.size() > (int)map.size() - k){
+//                 res.push_back(pq.top().second);
+//                 pq.pop();
+//             }
+//         }
+//         return res;
+//     }
 };
