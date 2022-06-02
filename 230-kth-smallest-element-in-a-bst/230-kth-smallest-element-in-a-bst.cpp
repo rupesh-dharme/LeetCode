5c6edcbcafa96afeb49ca983ef81b7c546e4f20a
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-02 21:02:44 +0530

    Time: 39 ms (5.06%), Space: 24.1 MB (88.27%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -9,18 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
-        int i = 0, res;
-        traverse(root, i, k, res);
+        int count = 0, res;
+        while (root) {
+            if (!root->left) {
+                count++;
+                if (count == k) {
+                    res = root->val;
+                }
+                root = root->right;
+            }
+            else {
+                TreeNode *prev = root->left;
+                while(prev->right != NULL && prev->right != root) prev = prev->right;
+                if (!prev->right) {
+                    prev->right = root;
+                    root = root->left;
+                } else {
+                    prev->right = NULL;
+                    count++;
+                    if (count == k) {
+                        res = root->val;
+                    }
+                    root = root->right;
+                }
+            }
+        }
         return res;
     }
-    void traverse(TreeNode* root, int &i, int k, int &res) {
-        if (!root) return;
-        if (root->left) traverse(root->left, i, k, res);
-        i++;
-        if (i == k) {res = root->val; return;}
-        if (root->right) traverse(root->right, i, k, res);
-    }
 };
+
+// class Solution {
+// public:
+//     int kthSmallest(TreeNode* root, int k) {
+//         int i = 0, res;
+//         traverse(root, i, k, res);
+//         return res;
+//     }
+//     void traverse(TreeNode* root, int &i, int k, int &res) {
+//         if (!root) return;
+//         if (root->left) traverse(root->left, i, k, res);
+//         i++;
+//         if (i == k) {res = root->val; return;}
+//         if (root->right) traverse(root->right, i, k, res);
+//     }
+// };
