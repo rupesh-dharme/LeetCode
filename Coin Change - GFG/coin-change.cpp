bbe467466f7768a0f82d8609133a4c6479db5296
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-03 15:10:19 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -3,17 +3,18 @@
 using namespace std;
 
  // } Driver Code Ends
+
+
 class Solution {
   public:
-    long long int count(int S[], int m, int n) {
-        vector<vector<long long int>> dp(m + 1, vector<long long int> (n + 1, 0));
+    long long int count(int s[], int m, int n) {
+        vector<vector<long long>> dp(m+1, vector<long long> (n+1, 0));
         for (int i = 0; i <= m; i++) dp[i][0] = 1;
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (S[i-1] <= j) {
-                    dp[i][j] = dp[i-1][j] + dp[i][j - S[i-1]];
-                }
-                else {
+                if (s[i-1] <= j) {
+                    dp[i][j] = dp[i][j-s[i-1]] + dp[i-1][j];
+                } else {
                     dp[i][j] = dp[i-1][j];
                 }
             }
@@ -21,6 +22,25 @@ class Solution {
         return dp[m][n];
     }
 };
+
+// class Solution {
+//   public:
+//     long long int count(int S[], int m, int n) {
+//         vector<vector<long long int>> dp(m + 1, vector<long long int> (n + 1, 0));
+//         for (int i = 0; i <= m; i++) dp[i][0] = 1;
+//         for (int i = 1; i <= m; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 if (S[i-1] <= j) {
+//                     dp[i][j] = dp[i-1][j] + dp[i][j - S[i-1]];
+//                 }
+//                 else {
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// };
 
 // { Driver Code Starts.
 int main() {
