d3dd19c8a3274249a729016539ab42bfe372f14f
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-03 13:08:00 +0530

    Time: 16 ms (58.18%), Space: 21.7 MB (71.59%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,17 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    bool isValidBST(TreeNode* root) {
-        TreeNode* prev = NULL;
-        return validate(root, prev);
-    }
-    bool validate(TreeNode* node, TreeNode* &prev) {
-        if (node == NULL) return true;
-        if (!validate(node->left, prev)) return false;
-        if (prev != NULL && prev->val >= node->val) return false;
-        prev = node;
-        return validate(node->right, prev);
+    bool isValidBST(TreeNode* root, long long l = (long long)INT_MIN-1, long long r = (long long)INT_MAX+1) {
+        if (!root) return true;
+        if (!(l < root->val) || !(root->val < r)) return false;
+        return isValidBST(root->left, l, root->val) && isValidBST(root->right, root->val, r);
     }
 };
+
+// class Solution {
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         TreeNode* prev = NULL;
+//         return validate(root, prev);
+//     }
+//     bool validate(TreeNode* node, TreeNode* &prev) {
+//         if (node == NULL) return true;
+//         if (!validate(node->left, prev)) return false;
+//         if (prev != NULL && prev->val >= node->val) return false;
+//         prev = node;
+//         return validate(node->right, prev);
+//     }
+// };
