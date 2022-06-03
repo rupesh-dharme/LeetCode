b6213f12ac83fe1cc6ada7135d4978b5a773c903
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-03 17:49:31 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -7,28 +7,81 @@ using namespace std;
  // } Driver Code Ends
 // User function Template for C++
 
+// class Solution{
+// public:
+//     int dp[101][101];
+//     Solution() {
+//         memset(dp, -1, sizeof(dp));
+//     }
+//     int matrixMultiplication(int N, int arr[])
+//     {
+//         int ans = solve(arr, 1, N-1);
+//         return ans;
+//     }
+//     int solve(int arr[], int i, int j) {
+//         if (i >= j) return 0;
+//         if (dp[i][j] != -1) return dp[i][j];
+//         int mn = INT_MAX;
+//         for (int k = i; k < j; k++) {
+//             int cost = solve(arr, i, k) + solve(arr, k+1, j) + (arr[i-1] * arr[k] * arr[j]);
+//             mn = min(mn, cost);
+//         }
+//         return dp[i][j] = mn;
+//     }
+// };
+
 class Solution{
-public:
+private:
     int dp[101][101];
-    Solution() {
-        memset(dp, -1, sizeof(dp));
-    }
+public:
     int matrixMultiplication(int N, int arr[])
     {
-        int ans = solve(arr, 1, N-1);
-        return ans;
+        memset(dp, -1, sizeof dp);
+        int res = solve(arr, 1, N-1);
+        return res;
     }
+    
     int solve(int arr[], int i, int j) {
         if (i >= j) return 0;
         if (dp[i][j] != -1) return dp[i][j];
         int mn = INT_MAX;
         for (int k = i; k < j; k++) {
-            int cost = solve(arr, i, k) + solve(arr, k+1, j) + (arr[i-1] * arr[k] * arr[j]);
-            mn = min(mn, cost);
+            int temp = solve(arr, i, k) + solve(arr, k+1, j) + arr[i-1] * arr[k] * arr[j];
+            mn = min(mn, temp);
         }
         return dp[i][j] = mn;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // { Driver Code Starts.
 
