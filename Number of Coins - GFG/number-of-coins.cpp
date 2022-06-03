a8afc3a2a213394a0a861e070b3b6d579fed48a1
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-03 15:38:50 +0530

    Added solution - LeetHub

--- Number of Coins - GFG/number-of-coins.cpp
+++ Number of Coins - GFG/number-of-coins.cpp
@@ -3,27 +3,46 @@
 using namespace std;
 
  // } Driver Code Ends
-class Solution{
 
+class Solution {
 	public:
 	int minCoins(int coins[], int M, int V) 
 	{ 
-	    vector<vector<int>> dp(M+1, vector<int> (V+1, INT_MAX));
+	    vector<vector<int>> dp(M+1, vector<int> (V+1, INT_MAX-1));
 	    for (int i = 0; i <= M; i++) dp[i][0] = 0;
 	    for (int i = 1; i <= M; i++) {
 	        for (int j = 1; j <= V; j++) {
 	            if (coins[i-1] <= j) {
-	                dp[i][j] = min(dp[i-1][j], dp[i][j-coins[i-1]] != INT_MAX ? dp[i][j-coins[i-1]] + 1 : INT_MAX);
-	            }
-	            else {
+	                dp[i][j] = min(dp[i][j-coins[i-1]] + 1, dp[i-1][j]);
+	            } else {
 	                dp[i][j] = dp[i-1][j];
 	            }
 	        }
 	    }
-	    return dp[M][V] != INT_MAX ? dp[M][V] : -1;
-	} 
-	  
+	    return dp[M][V] == INT_MAX-1 ? -1 : dp[M][V];
+	}
 };
+
+// class Solution {
+// 	public:
+// 	int minCoins(int coins[], int M, int V) 
+// 	{ 
+// 	    vector<vector<int>> dp(M+1, vector<int> (V+1, INT_MAX));
+// 	    for (int i = 0; i <= M; i++) dp[i][0] = 0;
+// 	    for (int i = 1; i <= M; i++) {
+// 	        for (int j = 1; j <= V; j++) {
+// 	            if (coins[i-1] <= j) {
+// 	                dp[i][j] = min(dp[i-1][j], dp[i][j-coins[i-1]] != INT_MAX ? dp[i][j-coins[i-1]] + 1 : INT_MAX);
+// 	            }
+// 	            else {
+// 	                dp[i][j] = dp[i-1][j];
+// 	            }
+// 	        }
+// 	    }
+// 	    return dp[M][V] != INT_MAX ? dp[M][V] : -1;
+// 	} 
+	  
+// };
 
 // { Driver Code Starts.
 int main() 
