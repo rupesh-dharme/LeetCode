202d80d1f971dcdcca0c68c1886a52a2f6c8e658
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-03 19:11:55 +0530

    Added solution - LeetHub

--- Palindromic patitioning - GFG/palindromic-patitioning.cpp
+++ Palindromic patitioning - GFG/palindromic-patitioning.cpp
@@ -8,37 +8,90 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-public:
+private:
     int dp[501][501];
-    Solution() {
-        memset(dp, -1, sizeof(dp));
-    }
-    
+public:
     int palindromicPartition(string str)
     {
-        int n = str.size();
-        int cuts = solve(str, 0, n-1);
-        return cuts;
+        memset(dp, -1, sizeof dp);
+        int res = solve(str, 0, str.size()-1);
+        return res;
     }
     
     int solve(string &str, int i, int j) {
-        if (i >= j || isPal(str, i, j)) return 0;
+        if (i >= j || palindrome(str, i, j)) return 0;
         if (dp[i][j] != -1) return dp[i][j];
-        int count = INT_MAX;
+        int mn = INT_MAX;
         for (int k = i; k < j; k++) {
             int temp = solve(str, i, k) + solve(str, k+1, j) + 1;
-            count = min(count, temp);
+            mn = min(temp, mn);
         }
-        return dp[i][j] = count;
+        return dp[i][j] = mn;
     }
     
-    bool isPal(string &s, int i, int j) {
+    bool palindrome(string &str, int i, int j) {
         while (i < j) {
-            if (s[i++] != s[j--]) return false;
+            if (str[i++] != str[j--]) return false;
         }
         return true;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution{
+// public:
+//     int dp[501][501];
+//     Solution() {
+//         memset(dp, -1, sizeof(dp));
+//     }
+    
+//     int palindromicPartition(string str)
+//     {
+//         int n = str.size();
+//         int cuts = solve(str, 0, n-1);
+//         return cuts;
+//     }
+    
+//     int solve(string &str, int i, int j) {
+//         if (i >= j || isPal(str, i, j)) return 0;
+//         if (dp[i][j] != -1) return dp[i][j];
+//         int count = INT_MAX;
+//         for (int k = i; k < j; k++) {
+//             int temp = solve(str, i, k) + solve(str, k+1, j) + 1;
+//             count = min(count, temp);
+//         }
+//         return dp[i][j] = count;
+//     }
+    
+//     bool isPal(string &s, int i, int j) {
+//         while (i < j) {
+//             if (s[i++] != s[j--]) return false;
+//         }
+//         return true;
+//     }
+// };
 
 // { Driver Code Starts.
 
