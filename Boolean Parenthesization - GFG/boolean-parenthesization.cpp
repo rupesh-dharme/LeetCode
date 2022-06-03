1c76561a0ddc25b98b2bf24078108b170035dac4
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-03 21:49:48 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -8,56 +8,126 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-public:
+private:
     int dp[201][201][2];
+public:
     int countWays(int N, string S){
-        memset(dp, -1, sizeof(dp));
-        int result = solve(S, 0, N-1, true);
-        return result;
+        memset(dp, -1, sizeof dp);
+        int res = solve(S, 0, S.size()-1, true);
+        return res;
     }
+    
     int solve(string &s, int i, int j, bool isTrue) {
-        if (i > j) return 0;
         if (i == j) {
-            if (s[i] == 'T') {
-                if (isTrue == true) return 1;
-                else return 0;
-            }
-            else {
-                if (isTrue == false) return 1;
-                else return 0;
-            }
+            if (s[i] == 'T') return isTrue == true;
+            else return isTrue == false;
         }
         if (dp[i][j][isTrue] != -1) return dp[i][j][isTrue];
-        int ans = 0;
-        for (int k = i+1; k < j; k++) {
-            int lt = solve(s, i, k-1, true);
-            int lf = solve(s, i, k-1, false);
-            int rt = solve(s, k+1, j, true);
-            int rf = solve(s, k+1, j, false);
+        int ways = 0;
+        for (int k = i+1; k < j; k+=2) {
+            int temp = 0;
             if (s[k] == '&') {
-                if (isTrue) {
-                    ans += (lt * rt);
-                } else {
-                    ans += (lf * rf + lt * rf + rt * lf);
+                if (isTrue) temp += solve(s, i, k-1, true) * solve(s, k+1, j, true);
+                else {
+                    temp += solve(s, i, k-1, false) * solve(s, k+1, j, false);
+                    temp += solve(s, i, k-1, true) * solve(s, k+1, j, false);
+                    temp += solve(s, i, k-1, false) * solve(s, k+1, j, true);
                 }
             } else if (s[k] == '|') {
                 if (isTrue) {
-                    ans += (lt * rt + lt * rf + rt * lf);
-                } else {
-                    ans += (lf * rf);
+                    temp += solve(s, i, k-1, true) * solve(s, k+1, j, true);
+                    temp += solve(s, i, k-1, true) * solve(s, k+1, j, false);
+                    temp += solve(s, i, k-1, false) * solve(s, k+1, j, true);
                 }
+                else temp += solve(s, i, k-1, false) * solve(s, k+1, j, false);
             } else {
                 if (isTrue) {
-                    ans += (lt * rf + lf * rt);
-                } else {
-                    ans += (lf * rf + lt * rt); 
+                    temp += solve(s, i, k-1, true) * solve(s, k+1, j, false);
+                    temp += solve(s, i, k-1, false) * solve(s, k+1, j, true);
+                }
+                else {
+                    temp += solve(s, i, k-1, true) * solve(s, k+1, j, true);
+                    temp += solve(s, i, k-1, false) * solve(s, k+1, j, false);
                 }
             }
-            ans = ans % 1003;
+            ways += temp;
         }
-        return dp[i][j][isTrue] = ans % 1003;
+        return dp[i][j][isTrue] = ways%1003;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution{
+// public:
+//     int dp[201][201][2];
+//     int countWays(int N, string S){
+//         memset(dp, -1, sizeof(dp));
+//         int result = solve(S, 0, N-1, true);
+//         return result;
+//     }
+//     int solve(string &s, int i, int j, bool isTrue) {
+//         if (i > j) return 0;
+//         if (i == j) {
+//             if (s[i] == 'T') {
+//                 if (isTrue == true) return 1;
+//                 else return 0;
+//             }
+//             else {
+//                 if (isTrue == false) return 1;
+//                 else return 0;
+//             }
+//         }
+//         if (dp[i][j][isTrue] != -1) return dp[i][j][isTrue];
+//         int ans = 0;
+//         for (int k = i+1; k < j; k++) {
+//             int lt = solve(s, i, k-1, true);
+//             int lf = solve(s, i, k-1, false);
+//             int rt = solve(s, k+1, j, true);
+//             int rf = solve(s, k+1, j, false);
+//             if (s[k] == '&') {
+//                 if (isTrue) {
+//                     ans += (lt * rt);
+//                 } else {
+//                     ans += (lf * rf + lt * rf + rt * lf);
+//                 }
+//             } else if (s[k] == '|') {
+//                 if (isTrue) {
+//                     ans += (lt * rt + lt * rf + rt * lf);
+//                 } else {
+//                     ans += (lf * rf);
+//                 }
+//             } else {
+//                 if (isTrue) {
+//                     ans += (lt * rf + lf * rt);
+//                 } else {
+//                     ans += (lf * rf + lt * rt); 
+//                 }
+//             }
+//             ans = ans % 1003;
+//         }
+//         return dp[i][j][isTrue] = ans % 1003;
+//     }
+// };
 
 // { Driver Code Starts.
 
