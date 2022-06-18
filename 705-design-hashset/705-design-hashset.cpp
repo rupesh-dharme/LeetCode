a3148cc6b8555d86d037baa2c2dff9fb7c596752
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-18 12:18:23 +0530

    Time: 118 ms (60.93%), Space: 41.8 MB (66.34%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,21 +1,32 @@
 class MyHashSet {
-    int hash[1000001];
 public:
+    vector<list<int>> set;
+    int size = 1000;
     MyHashSet() {
-        memset(hash, -1, sizeof(hash));
+        set.resize(size);
+    }
+    
+    int hash(int key) {
+        return key%size;
     }
     
     void add(int key) {
-        hash[key] = 1;
+        if (contains(key)) return;
+        int index = hash(key);
+        set[index].emplace_back(key);
     }
     
     void remove(int key) {
-        hash[key] = -1;
+        int index = hash(key);
+        auto it = find(set[index].begin(), set[index].end(), key);
+        if (it == set[index].end()) return;
+        set[index].erase(it);
     }
     
     bool contains(int key) {
-        if (hash[key] == 1) return true;
-        return false;
+        int index = hash(key);
+        if (find(set[index].begin(), set[index].end(), key) != set[index].end()) return true;
+        else return false;
     }
 };
 
