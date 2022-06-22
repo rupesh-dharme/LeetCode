60f7825645a7483210f1209750f2a85f30d701ae
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-22 17:25:27 +0530

    Time: 919 ms (12.39%), Space: 102.4 MB (7.25%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
-    int findMinArrowShots(vector<vector<int>>& points) {
-        if (points.size() == 0) return 0;
-        sort(points.begin(), points.end());
-        vector<int> p = points[0];
-        int count = 0, n = points.size();
+    int findMinArrowShots(vector<vector<int>>& points) { // sorting according to second value
+        int n = points.size();
+        if (n == 0) return 0;
+        sort(points.begin(), points.end(), [](vector<int> &a, vector<int> &b) {
+            return a[1] < b[1];
+        });
+        int count = 1;
+        int end = points[0][1]; // first's end
         for (int i = 1; i < n; i++) {
-            auto point = points[i];
-            if (point[0] <= p[1]) {
-                count++;
-                p[1] = min(p[1], point[1]);
+            vector<int> point = points[i];
+            if (point[0] <= end) {
+                
             } else {
-                p = point;
+                end = point[1];
+                count++;
             }
         }
-        return n - count;
+        return count;
     }
 };
+
+// class Solution {
+// public:
+//     int findMinArrowShots(vector<vector<int>>& points) { // sorting according to first value
+//         if (points.size() == 0) return 0;
+//         sort(points.begin(), points.end());
+//         vector<int> p = points[0];
+//         int count = 0, n = points.size();
+//         for (int i = 1; i < n; i++) {
+//             auto point = points[i];
+//             if (point[0] <= p[1]) {
+//                 count++;
+//                 p[1] = min(p[1], point[1]);
+//             } else {
+//                 p = point;
+//             }
+//         }
+//         return n - count;
+//     }
+// };
