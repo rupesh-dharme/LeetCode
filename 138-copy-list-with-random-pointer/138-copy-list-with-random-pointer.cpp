2ab4b6faf8bfd1e511db8182f2504fcd828e8c36
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-06-23 11:49:29 +0530

    Time: 12 ms (62.09%), Space: 11.3 MB (27.73%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -16,21 +16,63 @@ public:
 
 class Solution {
 public:
+    void crossJoin(Node *head) {
+        Node *node = head;
+        while (node) {
+            Node *next = node->next;
+            node->next = new Node(node->val);
+            node->next->next = next;
+            node = node->next->next;
+        }
+    }
+    
+    void attachRandom(Node *head) {
+        Node *node = head;
+        while (node) {
+            if (node->random)
+                node->next->random = node->random->next;
+            else node->next->random = NULL;
+            node = node->next->next;
+        }
+    }
+    
+    Node *seperate(Node *head) {
+        Node *node = head, *copy = head->next, *tail = head->next;
+        while (node) {
+            node->next = tail->next;
+            if (node->next)
+                tail->next = node->next->next;
+            else tail->next = NULL;
+            node = node->next;
+            tail = tail->next;
+        }
+        return copy;
+    }
+    
     Node* copyRandomList(Node* head) {
-        if (!head) return nullptr;
-        unordered_map<Node*, Node*> um;
-        Node* temp = head;
-        while (temp) {
-            um[temp] = new Node(temp->val);
-            temp = temp->next;
-        }
-        temp = head;
-        while (temp) {
-            um[temp]->next = um[temp->next];
-            um[temp]->random = um[temp->random];
-            temp = temp->next;
-        }
-        free(temp);
-        return um[head];
+        if (!head) return NULL;
+        crossJoin(head);
+        attachRandom(head);
+        return seperate(head);
     }
 };
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         unordered_map<Node *, Node *> um;
+//         Node *node = head;
+//         while (node) {
+//             Node *newNode = new Node(node->val);
+//             um[node] = newNode;
+//             node = node->next;
+//         }
+//         node = head;
+//         while(node) {
+//             um[node]->next = um[node->next];
+//             um[node]->random = um[node->random];
+//             node = node->next;
+//         }
+//         return um[head];
+//     }
+// };
