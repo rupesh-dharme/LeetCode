882c1da0554f21b423215f23a8897c94fcebd1c6
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-08-22 12:12:36 +0530

    Time: 12 ms (8.64%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n == 0) return false;
-        while (n % 4 == 0) n /= 4;
-        if (n == 1) return true;
+        if (n <= 0) return false;
+        if ((n & (n - 1)) == 0 && (__builtin_popcount(n - 1) % 2) == 0) return true;
         return false;
     }
+    // bool isPowerOfFour(int n) {
+    //     if (n == 0) return false;
+    //     while (n % 4 == 0) n /= 4;
+    //     if (n == 1) return true;
+    //     return false;
+    // }
 };
