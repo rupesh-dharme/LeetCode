480978a4a17cd3b2d8e6d23406ffeadf7a3e6885
Rupesh Dharme <74305445+rupesh-dharme@users.noreply.github.com>
2022-08-23 11:29:41 +0530

    Time: 449 ms (26.38%), Space: 118 MB (50.21%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -8,10 +8,11 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode *reverse(ListNode *head) {
-        ListNode *p = NULL, *c = head, *n;
+        ListNode *p = NULL, *c = head, *n = NULL;
         while (c) {
             n = c->next;
             c->next = p;
@@ -21,19 +22,53 @@ public:
         return p;
     }
     bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return head;
-        auto slow = head, fast = head;
+        if (!head || !head->next) return true;
+        ListNode *slow = head, *fast = head;
         while (fast->next && fast->next->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
         slow->next = reverse(slow->next);
-        auto start = head, mid = slow->next;
-        while (start && mid) {
-            if (start->val != mid->val) return false;
-            start = start->next;
-            mid = mid->next;
+        ListNode *a = head, *b = slow->next;
+        while (b) {
+            if (a->val != b->val) {
+                slow->next = reverse(slow->next);
+                return false;
+            }
+            a = a->next;
+            b = b->next;
         }
+        slow->next = reverse(slow->next);
         return true;
     }
 };
+
+// class Solution {
+// public:
+//     ListNode *reverse(ListNode *head) {
+//         ListNode *p = NULL, *c = head, *n;
+//         while (c) {
+//             n = c->next;
+//             c->next = p;
+//             p = c;
+//             c = n;
+//         }
+//         return p;
+//     }
+//     bool isPalindrome(ListNode* head) {
+//         if (!head || !head->next) return head;
+//         auto slow = head, fast = head;
+//         while (fast->next && fast->next->next) {
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         slow->next = reverse(slow->next);
+//         auto start = head, mid = slow->next;
+//         while (start && mid) {
+//             if (start->val != mid->val) return false;
+//             start = start->next;
+//             mid = mid->next;
+//         }
+//         return true;
+//     }
+// };
